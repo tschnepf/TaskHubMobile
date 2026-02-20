691beb4e7e258a24589b3a1f49d58ca22b3fae2c
tschnepf <timschnepf@gmail.com>
2026-02-20 13:57:20 -0700

    Initial Commit

---
+++ TaskHubMobile/TaskHubMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskHubMobileApp.swift
+//  TaskHubMobile
+//
+//  Created by tim on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskHubMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskHubMobileTests/TaskHubMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskHubMobileTests.swift
+//  TaskHubMobileTests
+//
+//  Created by tim on 2/20/26.
+//
+
+import Testing
+@testable import TaskHubMobile
+
+struct TaskHubMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
